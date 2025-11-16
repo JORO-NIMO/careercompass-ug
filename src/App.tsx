23f2ab20a2db4224ad870ad0ab3c462b386ceeed
JORO-NIMO <nimoconstructions@gmail.com>
2025-11-16 21:17:15 +0300

    fix(ui): remove Sonner toaster to eliminate bottom-left floating widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+// Sonner toaster removed to avoid bottom-left floating widget
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -26,8 +26,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <TooltipProvider>
-        <Toaster />
-        <Sonner />
+  <Toaster />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
